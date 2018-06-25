4d9dba3f8149d972d24735a99d72da9983645b53
José Luiz <joseluiz98.jl@gmail.com>
2018-06-24 23:11:24 -0300

    IP and Mask are received, treated and parsed to int

--- tp.py
+++ tp.py
@@ -1,1 +1,25 @@
-print Redes
+# ip = raw_input("Digite o IP: ")
+ip = '192.168.10.1'
+mask = '255.255.255.128'
+
+def breakOctets(ip):
+    vetOcteto = ''
+    vetBin = []
+    octetos = []
+    for idx, bit in enumerate(ip):
+        if(bit != '.'):
+            vetOcteto = vetOcteto + bit
+        if (bit == '.') or (idx == len(ip)-1):
+            print "cai no bit", bit
+            # print bin(vetOcteto)
+            octetos.append(vetOcteto)
+            vetBin.append(bin(int(vetOcteto)))
+            vetOcteto = ''
+    return octetos, vetBin
+
+ip, ipBin = breakOctets(ip)
+mask, ipMask = breakOctets(mask)
+
+if (len(ip) != 4):
+    print "Um ip possui 4 octetos! Interrompendo programa."
+    exit()
