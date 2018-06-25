c7d1539bea4855f07da22f81f726959c5dbdeee0
José Luiz <joseluiz98.jl@gmail.com>
2018-06-24 23:55:24 -0300

    some information about subnet and hosts already functional

--- tp.py
+++ tp.py
@@ -10,16 +10,36 @@ def breakOctets(ip):
         if(bit != '.'):
             vetOcteto = vetOcteto + bit
         if (bit == '.') or (idx == len(ip)-1):
-            print "cai no bit", bit
-            # print bin(vetOcteto)
             octetos.append(vetOcteto)
             vetBin.append(bin(int(vetOcteto)))
             vetOcteto = ''
     return octetos, vetBin
 
+# Funcao que recebe mascara em binario e extrai todas as informacoes que a mascara nos diz
+# A funcao calcula a partir da formula n = 2^x onde x seria o numero de bits emprestados no octeto incompleto
+# Lembrando que na Classe C somente o ultimo octeto pode ser imcompleto
+# Usa tambem a formula (2^n)-2 para calcular numero de hosts
+def extractInfoOfMask(mask):
+    nBitsForSubNet=0
+    nBitsForHosts=0
+    lastOctect = mask[3][2:]
+    for bit in lastOctect:
+        if(bit == '1'):
+            nBitsForSubNet=nBitsForSubNet+1
+        else:
+            nBitsForHosts=nBitsForHosts+1
+    return 2**(nBitsForSubNet), (2**(nBitsForHosts))-2, 2**nBitsForHosts, nBitsForHosts
+
+
 ip, ipBin = breakOctets(ip)
-mask, ipMask = breakOctets(mask)
+mask, maskBin = breakOctets(mask)
 
 if (len(ip) != 4):
     print "Um ip possui 4 octetos! Interrompendo programa."
     exit()
+
+numberOfSubNets, numberOfHosts, nIPsForSubNet, nBitsForHosts = extractInfoOfMask(maskBin)
+print "Bits para Hosts", nBitsForHosts
+print "Subredes", numberOfSubNets
+print "IPs por Subrede", nIPsForSubNet
+print "Hosts", numberOfHosts
